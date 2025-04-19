9c33e5a72de6414b5c085942877b90c0ef4340fa
kyohey735 <birdbird.0618@gmail.com>
2025-04-19 14:37:30 +0900

    Commit

--- day1/01_streamlit_UI/app.py
+++ day1/01_streamlit_UI/app.py
@@ -41,14 +41,14 @@ st.write(f"こんにちは、{name}さん！")
 #     st.success("ボタンがクリックされました！")
 
 # チェックボックス
-# st.subheader("チェックボックス")
-# if st.checkbox("チェックを入れると追加コンテンツが表示されます"):
-#     st.info("これは隠れたコンテンツです！")
+st.subheader("チェックボックス")
+if st.checkbox("チェックを入れると追加コンテンツが表示されます"):
+    st.info("これは隠れたコンテンツです！")
 
 # スライダー
-# st.subheader("スライダー")
-# age = st.slider("年齢", 0, 100, 25)
-# st.write(f"あなたの年齢: {age}")
+st.subheader("スライダー")
+age = st.slider("年齢", 0, 100, 25)
+st.write(f"あなたの年齢: {age}")
 
 # セレクトボックス
 # st.subheader("セレクトボックス")
